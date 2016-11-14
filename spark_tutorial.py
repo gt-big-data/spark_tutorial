87254feff4e94cdd86e30b9e5bc780bf7998e2b5
Faisal Animashaun <d.f.animashaun@gmail.com>
2016-11-14 14:41:59 -0500

    added svm code

--- spark_tutorial.py
+++ spark_tutorial.py
@@ -1,6 +1,9 @@
 from collections import namedtuple
 from csv import DictReader
+
 from pyspark import SparkContext
+from pyspark.mllib.classification import SVMWithSGD, SVMModel
+from pyspark.mllib.regression import LabeledPoint
 
 sc = SparkContext()
 Row = namedtuple('Row', ['year', 'occupation', 'show', 'group', 'guess_list'])
