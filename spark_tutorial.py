a2b13838704ed8af6886bc1ca3735b9bee82584f
Faisal Animashaun <d.f.animashaun@gmail.com>
2016-11-14 19:31:33 -0500

    added finished code

--- spark_tutorial.py
+++ spark_tutorial.py
@@ -9,3 +9,25 @@ sc = SparkContext()
 Row = namedtuple('Row', ['year', 'occupation', 'show', 'group', 'guess_list'])
 f = open('daily_show_guests.csv', 'r')
 reader = DictReader(f)
+
+def parse(line):
+    label, height, weight = line.split(',')
+    return LabeledPoint(float(label), [float(height), float(weight)])
+
+athletes = sc.textFile('athletes.csv') \
+    .map(parse)
+
+model = SVMWithSGD.train(athletes, iterations=1000)
+predictions = athletes.map(lambda p: (p.label, model.predict(p.features)))
+accuracy = predictions.filter(lambda (v, p): v != p)
+
+print 'Error: {}'.format(accuracy.count() / float(athletes.count()))
+
+# daily_show = sc.parallelize([Row(**r) for r in reader])
+
+# guests = daily_show.map(lambda show: (show.occupation, 1)) \
+#     .reduceByKey(lambda x, y: x + y)
+# """
+# [ (1991, 1)]
+# """
+# print guests.take(guests.count())
